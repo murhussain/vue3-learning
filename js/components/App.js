1e58e3113b37ac55c2bb235fddf8153b521916ae
Murhussain <husseinmurashi@yahoo.com>
2023-03-22 16:02:32 +0200

    Copying codes from backup file into app.js

--- js/components/App.js
+++ js/components/App.js
@@ -1,7 +1,53 @@
-import AppButton from './appButton.js';
-
 export default {
-  components: {
-    'app-button': AppButton
+  template: `
+    <section v-if="inProgressAssignment.length">
+      <h1 class="font-bold mb-2">In Progress Assignments</h1>
+      <ul>
+        <li 
+          v-for="assignment in inProgressAssignment" 
+          :key="assignment.id"
+        >
+          <label>
+            {{assignment.name}}
+            <input type="checkbox" v-model="assignment.completed">
+          </label>
+        </li>
+      </ul>
+    </section>
+
+    <section v-if="completedAssignment.length" class="mt-8">
+      <h1 class="font-bold mb-2">Completed Assignments</h1>
+      <ul>
+        <li 
+          v-for="assignment in completedAssignment" 
+          :key="assignment.id"
+        >
+          <label>
+            {{assignment.name}}
+            <input type="checkbox" v-model="assignment.completed">
+          </label>
+        </li>
+      </ul>
+    </section>
+  `,
+
+  data() {
+    return {
+      assignments: [
+        { id: 1, name: 'Finish project', completed: false },
+        { id: 2, name: 'Read chapter 4', completed: false },
+        { id: 2, name: 'Turn in homework', completed: false }
+      ]
+    }
+  },
+
+  computed: {
+    inProgressAssignment() {
+      return this.assignments.filter(a => !a.completed);
+    },
+
+    completedAssignment() {
+      return this.assignments.filter(a => a.completed);
+    }
   }
 };
