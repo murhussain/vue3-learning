3ccb23e7b66314fe25abdd7246dfaa35d32c99f1
Murhussain <husseinmurashi@yahoo.com>
2023-03-24 11:49:42 +0200

    Adding panelCard to my assignment-lists component

--- js/components/App.js
+++ js/components/App.js
@@ -1,48 +1,11 @@
 import Assignments from "./Assignments.js";
-import Panel from "./Panel.js";
 
 export default {
-  components: { Assignments, Panel },
+  components: { Assignments },
 
   template: `
     <div class="grid gap-6">
-      <Assignments></Assignments>
-      <panel>
-        this is my default content
-      </panel>
-
-      <panel>
-        <template v-slot:heading>
-          Hey there
-        </template>
-        <template v-slot:default>
-          this is my default content
-        </template>
-      </panel>
-
-      <panel>
-        <template v-slot:heading>
-          Hey there
-        </template>
-        
-        this is my default content
-
-        <template v-slot:footer>
-          Click here to learn more....
-        </template>
-      </panel>
-
-      <panel theme="light">
-        <template v-slot:heading>
-          Hey there
-        </template>
-        
-        this is my default content
-
-        <template v-slot:footer>
-          Click here to learn more....
-        </template>
-      </panel>
+      <Assignments></Assignments
     </div>
   `,
 }
